8b317d74ae39ea1e9f8a40eda59eb778cbb1c371
Vinayak Kulkarni <vinayakkulkarni@users.noreply.github.com>
2016-03-02 21:10:55 +0530

    Updated navbar and footer
    Still fixing stuff :D

--- view.php
+++ view.php
@@ -17,6 +17,16 @@ include_once("db.php");
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="view.php">View Blog Posts</a></li>
+        <li><a href="post.php">Create a New Blog Post</a></li>
+      </ul>
+    </div>
+  </nav>      
 <div class="container">
 <?php
 	$sql = "SELECT * FROM posts ORDER BY id DESC";
@@ -60,5 +70,30 @@ $db->close();
 <br />
 <a href="post.php" target="_blank" class="waves-effect waves-light btn blue">Create a New Post</a>
 </div>
+<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            &copy; 2016 Vinayak R K.
+            <a class="grey-text text-lighten-4 right" href="#!">Gonna social icons here</a>
+            </div>
+          </div>
+        </footer>
 </body>
 </html>
